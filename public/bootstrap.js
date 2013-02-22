49f24c202f16bc4a55e70bf850c50656ca90aba4
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-02-22 11:43:09 -0500

    hack for tooltip positioning. whatever

--- public/bootstrap.js
+++ public/bootstrap.js
@@ -142,7 +142,8 @@
             tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}
             break
           case 'top':
-            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}
+            // HACK FUCK MY LIFE
+            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 3 + 10}
             break
           case 'left':
             tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}
