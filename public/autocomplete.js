612b49bfd060d074b89d7991f7dc85c28fa3d2fb
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-02-22 12:34:42 -0500

    disable submission of non-results

--- public/autocomplete.js
+++ public/autocomplete.js
@@ -7,6 +7,8 @@ function initiateAutocomplete() {
         minLength: 2,
         delay: 125,
         parent: jQuery('.search-form, .search-structure'),
+        see_all: false,
+        top: false,
         select:    function(item) {
           var slug = item.attr("href").replace("/", "").replace("-tickets/", "");
           MT.fire(slug);
