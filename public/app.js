f620ee3d968e08b74a3934842d8d3fe57421883f
Ben Clark <benjamin.k.clark@gmail.com>
2013-02-22 02:02:12 -0500

    tidying up and moving my functions around

--- public/app.js
+++ public/app.js
@@ -96,6 +96,7 @@
           html = "<div class='music-tour-timeline-points' style='margin-left: " + Math.round(pixelShift) + "px; z-index:" + (20-index) + "' data-eventid='" + evt.id + "'>" + (index + 1) + "</div>";
 
       $(".music-tour-timeline").append(html);
+      getSeoData(events);
 
       function tooltip() {
         var that = $(d3.select(this));
@@ -136,7 +137,66 @@
         .text(function(d) { return index + 1; });
     });
 
-    // get seo data
+    // do things on click of backward/forward arrows
+    $(".music-tour-timeline-arrow").click(function() {
+      var direction = $(this).hasClass("music-tour-forward") ? "forward" : $(this).hasClass("music-tour-backward") ? "backward" : "";
+      shiftActivePoint(direction);
+    });
+    
+    // do things on mouseenter of timeline points
+    $(".music-tour-timeline-points").mouseenter(function() {
+      activatePoints($(this).attr("data-eventid"));
+      writeSeoData($(this).data("info"));
+    });
+
+    // do things on mouseenter of map points
+    $("svg .music-tour-label, svg .music-tour-points").hover(function() {
+      activatePoints($(this).attr("data-eventid"));
+      writeSeoData($(this).data("info"));
+    });
+
+    function shiftActivePoint(fwdOrBkwd) {
+      var x = (fwdOrBkwd == "forward") ? 1 : "backward" ? -1 : 0;
+      var o = findIndexOfActivePoint();
+      switch(x + o) {
+        case 7:
+          clearPage();
+          drawPage(1);
+          break;
+        case -1: 
+          clearPage();
+          drawPage(0);
+          break;
+        default:
+          activatePoints(o.event_ids[o.active_index + x]);
+      }
+    }
+
+    function findIndexOfActivePoint() {
+      var activeId = $(".music-tour-timeline-points.active").data("eventid");
+      var eventIds = _.pluck(events, "id");
+      return {
+        "active_index": eventIds.indexOf(activeId),
+        "event_ids": eventIds
+      };
+    }
+
+  }
+
+  function activatePoints(eventId) {
+    // first deactivate all points
+    $("svg .music-tour-points, .music-tour-timeline-points").each(function() {
+      $(this).attr("class", $(this).attr("class").replace(" active",""));
+    });
+
+    // then activate the correct point on the timeline & map
+    $(".music-tour-timeline-points[data-eventid='" + eventId + "'], svg .music-tour-points.event-" + eventId).each(function() {
+      $(this).attr("class", $(this).attr("class") + " active");
+    });
+  }
+  
+  // get seo data
+  function getSeoData(events) {
     var seoUrl = "http://seatgeek.com/utility/mapseo?performer_id=918";
     var urlPieces = _.map(events, function(e, index) {
       return "&venues[" + index + "][state]=" + e.venue.state + "&venues[" + index + "][id]=" + e.venue.id;
@@ -202,118 +262,72 @@
       },
       dataType: "jsonp"
     });
-
-    // do things on mouseenter of timeline points
-    $(".music-tour-timeline-points").mouseenter(function() {
-      activatePoints($(this).attr("data-eventid"));
-      writeSeoData($(this).data("info"));
-    });
-
-    // do things on mouseenter of map points
-    $("svg .music-tour-label, svg .music-tour-points").hover(function() {
-      activatePoints($(this).attr("data-eventid"));
-      writeSeoData($(this).data("info"));
-    });
-
-    $(".music-tour-timeline-arrow").click(function() {
-      var direction = $(this).hasClass("music-tour-forward") ? "forward" : $(this).hasClass("music-tour-backward") ? "backward" : "";
-      shiftActivePoint(direction);
-    });
-
-
-    function shiftActivePoint(fwdOrBkwd) {
-      var x = (fwdOrBkwd == "forward") ? 1 : "backward" ? -1 : 0;
-      var o = findIndexOfActivePoint();
-      activatePoints(o.event_ids[o.active_index + x]);
-    }
-
-    function findIndexOfActivePoint() {
-      var activeId = $(".music-tour-timeline-points.active").data("eventid");
-      var eventIds = _.pluck(events, "id");
-      return {
-        "active_index": eventIds.indexOf(activeId),
-        "event_ids": eventIds
-      };
-    }
-
-    function activatePoints(eventId) {
-      // first deactivate all points
-      $("svg .music-tour-points, .music-tour-timeline-points").each(function() {
-        $(this).attr("class", $(this).attr("class").replace(" active",""));
-      });
-
-      // then activate the correct point on the timeline & map
-      $(".music-tour-timeline-points[data-eventid='" + eventId + "'], svg .music-tour-points.event-" + eventId).each(function() {
-        $(this).attr("class", $(this).attr("class") + " active");
-      });
-    }
-
-    function writeSeoData(thisObjInfo) {
-      var openerHtml = _.map(thisObjInfo.openers, function(o, i) {
-        return "<a href='http://seatgeek.com/" + o.slug + "-tickets'>" + o.short_name + "</a>";
-      });
-      $(".music-tour-openers").html(openerHtml.join(", "));
-      $(".music-tour-performer-name").text(thisObjInfo.performer_short_name);
-      $(".music-tour-venue-name").text(thisObjInfo.venue_name);
-      $(".music-tour-state-name").text(thisObjInfo.venue_state);
-      $(".music-tour-days-since-venue").text(thisObjInfo.days_since_venue);
-      $(".music-tour-days-since-state").text(thisObjInfo.days_since_state);
-    }
-
-    var states = {
-      'AL': 'Alabama',
-      'AK': 'Alaska',
-      'AZ': 'Arizona',
-      'AR': 'Arkansas',
-      'CA': 'California',
-      'CO': 'Colorado',
-      'CT': 'Connecticut',
-      'DE': 'Delaware',
-      'DC': 'Washington, DC',
-      'FL': 'Florida',
-      'GA': 'Georgia',
-      'HI': 'Hawaii',
-      'ID': 'Idaho',
-      'IL': 'Illinois',
-      'IN': 'Indiana',
-      'IA': 'Iowa',
-      'KS': 'Kansas',
-      'KY': 'Kentucky',
-      'LA': 'Louisiana',
-      'ME': 'Maine',
-      'MD': 'Maryland',
-      'MA': 'Massachusetts',
-      'MI': 'Michigan',
-      'MN': 'Minnesota',
-      'MS': 'Mississippi',
-      'MO': 'Missouri',
-      'MT': 'Montana',
-      'NE': 'Nebraska',
-      'NV': 'Nevada',
-      'NH': 'New Hampshire',
-      'NJ': 'New Jersey',
-      'NM': 'New Mexico',
-      'NY': 'New York',
-      'NC': 'North Carolina',
-      'ND': 'North Dakota',
-      'OH': 'Ohio',
-      'OK': 'Oklahoma',
-      'OR': 'Oregon',
-      'PA': 'Pennsylvania',
-      'RI': 'Rhode Island',
-      'SC': 'South Carolina',
-      'SD': 'South Dakota',
-      'TN': 'Tennessee',
-      'TX': 'Texas',
-      'UT': 'Utah',
-      'VT': 'Vermont',
-      'VA': 'Virginia',
-      'WA': 'Washington',
-      'WV': 'West Virginia',
-      'WI': 'Wisconsin',
-      'WY': 'Wyoming'
-    };
-
   }
+
+  function writeSeoData(thisObjInfo) {
+    var openerHtml = _.map(thisObjInfo.openers, function(o, i) {
+      return "<a href='http://seatgeek.com/" + o.slug + "-tickets'>" + o.short_name + "</a>";
+    });
+    $(".music-tour-openers").html(openerHtml.join(", "));
+    $(".music-tour-performer-name").text(thisObjInfo.performer_short_name);
+    $(".music-tour-venue-name").text(thisObjInfo.venue_name);
+    $(".music-tour-state-name").text(thisObjInfo.venue_state);
+    $(".music-tour-days-since-venue").text(thisObjInfo.days_since_venue);
+    $(".music-tour-days-since-state").text(thisObjInfo.days_since_state);
+  }
+
+  var states = {
+    'AL': 'Alabama',
+    'AK': 'Alaska',
+    'AZ': 'Arizona',
+    'AR': 'Arkansas',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DE': 'Delaware',
+    'DC': 'Washington, DC',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'HI': 'Hawaii',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'IA': 'Iowa',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'ME': 'Maine',
+    'MD': 'Maryland',
+    'MA': 'Massachusetts',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MS': 'Mississippi',
+    'MO': 'Missouri',
+    'MT': 'Montana',
+    'NE': 'Nebraska',
+    'NV': 'Nevada',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NY': 'New York',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VT': 'Vermont',
+    'VA': 'Virginia',
+    'WA': 'Washington',
+    'WV': 'West Virginia',
+    'WI': 'Wisconsin',
+    'WY': 'Wyoming'
+  };
 
 })();
